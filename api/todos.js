40cafc4d200b7222ed46f6094ca388930fe613f7
yeon <parkwlfod@naver.com>
2024-09-24 09:48:22 +0900

    edit

--- api/todos.js
+++ api/todos.js
@@ -6,57 +6,58 @@ const dbName = 'todos';
 const url = 'mongodb+srv://parkwlfod:EALzqyrWQZRjZEMM@cluster0.q1xf9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const client = new MongoClient(url);
 
+let collection;
 async function connect(){
     await client.connect();
     // console.log('Connected successfully to server');
     const db = client.db(dbName);
-
-    return db.collection('data');
+    collection = db.collection('data');
+    return;
 }
-
+//client.close()로 끊고 다시 가고 하면 느려져서 모든 close없애기
 todos.get('/', async function (req, res) {
-    const collection = await connect();  //DB 접근
+    await connect();
+     // const collection = await connect(); DB 접근
     const findResult = await collection.find({}).toArray();  //DB 작업
-    client.close(); //DB 끊기 , 값이 findResult에 담겨있기 때문에 DB 끊어도 값 안사라짐
-
+    //client.close(); DB 끊기 , 값이 findResult에 담겨있기 때문에 DB 끊어도 값 안사라짐
      res.send( findResult )
 })
 todos.get('/:id',async function (req, res) {
     let id = req.params;
-   
-    const collection = await connect();  
+    // const collection = await connect();  
     const findResult = await collection.find(id).toArray(); 
-    client.close();
-
+    // client.close();
     res.send(findResult)
 })
 
 todos.post('/',async function (req, res) {
-    const collection = await connect();
-                       await collection.insertOne(req.body);
+    await collection.insertOne(req.body);
+    // const collection = await connect();
+    //                    await collection.insertOne(req.body);
     // const findResult = await collection.find({}).toArray();                
-    client.close();
+    // client.close();
     // res.send('req.body');
     res.send('done');
 })
 
 //(req, res) 첫번쨰 인자인 req에는 값을 , res는 값을 보내고
 todos.put('/', async function (req, res) {
-    const collection = await connect();
-                       await collection.updateOne({id:req.body.id},{$set:req.body});
+    await collection.updateOne({id:req.body.id},{$set:req.body});
+    // const collection = await connect();
+    //                    await collection.updateOne({id:req.body.id},{$set:req.body});
     // const findResult = await collection.find({}).toArray();
-    client.close();
+    // client.close();
 
     res.send('done');
 })
 
 todos.delete('/',async function (req, res) {
   //  let {id} = req.query; req.body가 아니라 req.query는 ?뒤에가 잡힘 -> {id:1}
-
-    const collection = await connect();
-                       await collection.deleteOne(req.query);
+    await collection.deleteOne(req.query);
+    // const collection = await connect();
+    //                    await collection.deleteOne(req.query);
     // const findResult = await collection.find({}).toArray();
-    client.close();
+    // client.close();
 
     res.send('done');
 })
---
+++ api/todos1.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const todos = express.Router()
+const { MongoClient } = require('mongodb');
+
+const dbName = 'todos';
+const url = 'mongodb+srv://yicha7:test1234@cluster2.kjcecrw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2';
+const client = new MongoClient(url);
+
+let collection;
+async function connect(){
+    await client.connect();
+    const db = client.db(dbName);
+    collection = db.collection('data');
+    return;
+}
+
+todos.get('/', async function (req, res) {
+    await connect();
+    const findResult = await collection.find({}).toArray();
+    res.send( findResult )
+})
+
+todos.get('/:id', async function (req, res) {
+    let id = req.params;
+    const findResult = await collection.find(id).toArray();
+
+    res.send( findResult )
+})
+
+todos.post('/', async function (req, res) {
+    await collection.insertOne(req.body);
+    res.send('done');
+})
+
+
+todos.put('/', async function (req, res) {
+    await collection.updateOne({id:req.body.id},{$set:req.body});
+    res.send('done');
+})
+
+todos.delete('/', async function (req, res) {
+    await collection.deleteOne(req.query);
+    res.send('done');
+})
+
+
+module.exports = todos;
